9f58f60336509a09ded42c48f4bbfdaa9d47c43b
Thomas Liske <thomas@fiasko-nw.net>
2023-06-04 23:40:38 +0200

    display/ws_epd: fix image drawing and clear screen on exit

--- birdprobe/display/ws_epd/__init__.py
+++ birdprobe/display/ws_epd/__init__.py
@@ -1,19 +1,30 @@
+import atexit
+import signal
 import epaper
 import time
-from birdprobe.display import AbstractDisplay
+from birdprobe.display import AbstractGraphicDisplay
 
-class WaveshareEPD(AbstractDisplay):
+class WaveshareEPD(AbstractGraphicDisplay):
     def __init__(self, birdprobe, display_name, display_config):
-        super().__init__(birdprobe, display_name, display_config)
-
         self.epd = epaper.epaper('epd2in7').EPD()
         self.epd.init()
-        self.epd.Clear(0)
-        time.sleep(2)
         self.epd.Clear(0xFF)
-        time.sleep(1)
+        self.epd.sleep()
+
+        super().__init__(birdprobe, display_name, display_config, self.epd.width, self.epd.height)
+
+    def draw_image(self, image):
+        self.epd.init()
+        self.epd.display(self.epd.getbuffer(image))
+        self.epd.sleep()
+
+    def clear_screen(self, *args):
+        self.epd.init()
+        self.epd.Clear(0xFF)
         self.epd.sleep()
 
 def run(birdprobe, display_name, display_config):
     display = WaveshareEPD(birdprobe, display_name, display_config)
+    atexit.register(display.clear_screen)
+    signal.signal(signal.SIGTERM, display.clear_screen)
     display.run()
