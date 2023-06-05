35158539bfe7c78f6ee73c2cccee97ff80d442aa
Thomas Liske <thomas@fiasko-nw.net>
2023-06-05 21:54:21 +0200

    display/ws_epd: add partial update support

--- birdprobe/display/ws_epd/__init__.py
+++ birdprobe/display/ws_epd/__init__.py
@@ -1,8 +1,9 @@
 import atexit
-import signal
-import epaper
-import time
 from birdprobe.display import AbstractGraphicDisplay
+import epaper
+from PIL import ImageChops
+import signal
+import time
 
 class WaveshareEPD(AbstractGraphicDisplay):
     def __init__(self, birdprobe, display_name, display_config):
@@ -10,12 +11,33 @@ class WaveshareEPD(AbstractGraphicDisplay):
         self.epd.init()
         self.epd.Clear(0xFF)
         self.epd.sleep()
+        self.has_partial_updates = hasattr(self.epd, 'display_Partial') and callable(self.epd.display_Partial)
+        self.last_image = None
+
+        print("Display partial update support: {}".format(self.has_partial_updates))
 
         super().__init__(birdprobe, display_name, display_config, self.epd.width, self.epd.height)
 
     def draw_image(self, image):
         self.epd.init()
-        self.epd.display(self.epd.getbuffer(image))
+
+        full_refresh = True
+        if self.last_image is not None:
+            diff = ImageChops.difference(self.last_image, image)
+            if diff is None:
+                # skip update if last and current image are identical
+                return
+
+            if self.has_partial_updates:
+                full_refresh = (diff[2] - diff[0]) > self.display_width / 2 and (diff[3] - diff[1]) > self.display_height / 2
+                if not full_refresh:
+                    self.epd.display_Partial(self.epd.getbuffer(image), *diff)
+
+        self.last_image = image
+
+        if full_refresh:
+            self.epd.display(self.epd.getbuffer(image))
+
         self.epd.sleep()
 
     def clear_screen(self, *args):
